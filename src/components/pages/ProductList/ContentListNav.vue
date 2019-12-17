88e8040d837a2d31331ce140a7e474cb3e28aba8
田仲展 <ts02690140@gmail.com>
2019-12-18 00:07:22 +0800

    12180007

--- src/components/pages/ProductList/ContentListNav.vue
+++ src/components/pages/ProductList/ContentListNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="content-list-nav">
-    <div class="d-flex top-nav">
+    <div class="top-nav">
       <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
@@ -23,55 +23,51 @@
           </template>
         </v-select>
 
-        <a href="" class="arrange active">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-          >
+        <a href class="arrange active">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
             <rect class="four" width="14" height="14" />
-            <rect
-              class="four"
-              width="14"
-              height="14"
-              transform="translate(18)"
-            />
-            <rect
-              class="four"
-              width="14"
-              height="14"
-              transform="translate(0 18)"
-            />
-            <rect
-              class="four"
-              width="14"
-              height="14"
-              transform="translate(18 18)"
-            />
+            <rect class="four" width="14" height="14" transform="translate(18)" />
+            <rect class="four" width="14" height="14" transform="translate(0 18)" />
+            <rect class="four" width="14" height="14" transform="translate(18 18)" />
           </svg>
         </a>
-        <a href="" class="arrange"
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-          >
+        <a href class="arrange">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
             <rect class="three" width="9" height="32" />
-            <rect
-              class="three"
-              width="9"
-              height="32"
-              transform="translate(11.5)"
-            />
-            <rect
-              class="three"
-              width="9"
-              height="32"
-              transform="translate(23)"
-            /></svg
-        ></a>
+            <rect class="three" width="9" height="32" transform="translate(11.5)" />
+            <rect class="three" width="9" height="32" transform="translate(23)" />
+          </svg>
+        </a>
+      </div>
+    </div>
+
+    <div class="mobile-nav">
+      <div class="d-flex btns">
+        <button>
+          <img src="~@/assets/img/icons/icn_arrow_d_g2.svg" alt />
+          <span>分類</span>
+        </button>
+        <button>
+          <img src="~@/assets/img/icons/组件 33 – 3.svg" alt />
+          <span>篩選</span>
+        </button>
+        <button>
+          <img src="~@/assets/img/icons/组件 34 – 3.svg" alt />
+          <span>排序</span>
+        </button>
+      </div>
+      <div class="d-flex the-array">
+        <a href class="arrange">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+            <rect class="two" width="9" height="20" rx="1" />
+            <rect class="two" width="9" height="20" rx="1" transform="translate(11)" />
+          </svg>
+        </a>
+        <a href class="arrange">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+            <rect class="one" width="20" height="20" rx="1" />
+          </svg>
+        </a>
       </div>
     </div>
   </div>
