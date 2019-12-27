60894edf8d791f18fd72c25aaafac0a6c48ffc86
Clifford <ts02690140@gmail.com>
2019-12-27 16:43:21 +0800

    12271645

---
+++ src/components/library/Carousel.vue
@@ -0,0 +1,23 @@
+<script>
+import carousel from "vue-owl-carousel";
+
+let mounted = carousel.mounted;
+
+export default Object.assign(carousel, {
+  mounted: function() {
+    mounted.bind(this).call();
+    if (this.loop) {
+      this.showPrev = true;
+    }
+  }
+});
+// export default {
+//   mounted: function() {
+//     mounted.bind(this).call();
+//     if (this.loop) {
+//       this.showPrev = true;
+//     }
+//   },
+//   ...carousel
+// };
+</script>
